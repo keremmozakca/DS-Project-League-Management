191580c359503f11d0d8373eaf94ae37d313cf11
Kerem Özakça <keremmozakca@gmail.com>
2024-12-23 16:36:26 +0300

    Created Project

---
+++ LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/LeagueManagementProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.leaguemanagementproject;
+
+/**
+ *
+ * @author keremozakca
+ */
+public class LeagueManagementProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
