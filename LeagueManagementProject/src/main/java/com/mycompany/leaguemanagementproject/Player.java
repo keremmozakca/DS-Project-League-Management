67790c84c49b3c65517ead828ae116cec63a4f57
Kerem Özakça <keremmozakca@gmail.com>
2024-12-23 17:15:14 +0300

    Player class updated

--- LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/Player.java
+++ LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/Player.java
@@ -1,4 +1,32 @@
 package com.mycompany.leaguemanagementproject;
+
 public class Player {
     
+    public String playerName;
+    public int playerID;
+    public String position;
+    public int goals;
+    
+    public Player(String playerName, int id, String position){
+        this.playerName = playerName;
+        this.playerID = id;
+        this.position = position;
+        this.goals = 0;
+    }
+    
+    public void updatePlayerName(String new_name){
+        this.playerName = new_name;
+    }
+    
+    public void updatePosition(String new_position){
+        this.position = new_position;
+    }
+    
+    public void updateGoals(int newGoal){
+        this.goals = newGoal;
+    }
+    
+    public void addGoal(){
+        this.goals++;
+    }
 }
