9919625400ecaeee0510ed8234a9079640198fb8
Fevzi Bağrıaçık <fevzi.bagriacik1905@gmail.com>
2024-12-23 16:53:58 +0300

    Team class is created

--- LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/LeagueManagementProject.java
+++ LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/LeagueManagementProject.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.leaguemanagementproject;
-
-/**
- *
- * @author keremozakca
- */
 public class LeagueManagementProject {
 
     public static void main(String[] args) {
---
+++ LeagueManagementProject/src/main/java/com/mycompany/leaguemanagementproject/Team.java
@@ -0,0 +1,18 @@
+package com.mycompany.leaguemanagementproject;
+public class Team {
+    public String teamName;
+    public int teamId;
+    public Player headPlayer;
+    public Player tailPlayer;
+    public int totalPoints;
+    public int goalDiff;
+    
+    public Team(String teamName, int teamId){
+        this.teamName = teamName;
+        this.teamId = teamId;
+        this.headPlayer = null;
+        this.tailPlayer = null;
+        this.totalPoints = 0;
+        this.goalDiff = 0;
+    }
+}
